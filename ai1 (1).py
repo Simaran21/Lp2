7afc81f348f13347bb4b470891fa96401ec6d3f3
Simaran21 <167753747+Simaran21@users.noreply.github.com>
2024-05-08 22:16:38 +0530

    Add files via upload

---
+++ ai1 (1).py
@@ -0,0 +1,33 @@
+
+
+# DFS algorithm in Python
+
+
+def dfs(graph, start, visited=None):
+	if visited is None:
+		visited = set()
+	visited.add(start)
+
+	print(start)
+
+	for next in graph[start] - visited:
+		dfs(graph, next, visited)
+	return visited
+
+graph = {'0': set(['1', '2']),
+'1': set(['0', '3', '4']),
+'2': set(['0']),
+'3': set(['1']),
+'4': set(['2', '3'])}
+
+dfs(graph, '0')
+
+#output
+"""(base) student@student:~/Documents$ python3 sid1.py
+0
+2
+1
+3
+4
+(base) student@student:~/Documents$ 
+"""
